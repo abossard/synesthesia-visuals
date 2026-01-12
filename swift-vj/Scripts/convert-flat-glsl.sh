63147fdccd175322558c282449acfd4ead33950d
Andre Bossard <anbossar@microsoft.com>
2026-01-12 20:39:07 +0100

    Add new uniform variables and update GLSL compatibility macros for enhanced shader functionality
    Signed-off-by: Andre Bossard <anbossar@microsoft.com>

--- swift-vj/Scripts/convert-flat-glsl.sh
+++ swift-vj/Scripts/convert-flat-glsl.sh
@@ -35,6 +35,10 @@ layout(binding = 0) uniform Uniforms {
     float beat;
     float energyFast;
     float energySlow;
+    float bin0;
+    float bin1;
+    float bin2;
+    float zoom;
 } _uniforms_;
 
 layout(binding = 1) uniform sampler2D backbuffer;
@@ -42,16 +46,29 @@ layout(binding = 1) uniform sampler2D backbuffer;
 layout(location = 0) out vec4 fragColor;
 
 // Y-flip for Metal coordinate system compatibility
-// Metal has Y=0 at top, OpenGL/Shadertoy has Y=0 at bottom
-// MUST be defined BEFORE the macros to avoid _uniforms_.resolution expansion
 vec4 _flipped_FragCoord() {
     return vec4(gl_FragCoord.x, _uniforms_.resolution.y - gl_FragCoord.y, gl_FragCoord.zw);
 }
 
-// GLSL compatibility - remap old uniforms to block members
+// GLSL compatibility - remap uniforms to block members
 #define time _uniforms_.time
 #define resolution _uniforms_.resolution
 #define mouse _uniforms_.mouse
+#define speed _uniforms_.speed
+#define bass _uniforms_.bass
+#define lowMid _uniforms_.lowMid
+#define mid _uniforms_.mid
+#define highs _uniforms_.highs
+#define level _uniforms_.level
+#define kickEnv _uniforms_.kickEnv
+#define kickPulse _uniforms_.kickPulse
+#define beat _uniforms_.beat
+#define energyFast _uniforms_.energyFast
+#define energySlow _uniforms_.energySlow
+#define bin0 _uniforms_.bin0
+#define bin1 _uniforms_.bin1
+#define bin2 _uniforms_.bin2
+#define zoom _uniforms_.zoom
 
 // texture2D -> texture for GLSL 4.5
 #define texture2D texture
@@ -77,13 +94,21 @@ sed -E \
     -e 's/gl_FragColor/fragColor/g' \
     -e 's/gl_FragCoord/_flipped_FragCoord()/g' \
     -e 's/vec2 mouse([[:space:]]*=[[:space:]])/vec2 _mouse\1/g' \
-    -e 's/float time([[:space:]]*=[[:space:]])/float _time\1/g' \
+    -e 's/float time([[:space:]]*=)/float _localTime\1/g' \
+    -e 's/float time([[:space:]]*,)/float _t\1/g' \
+    -e 's/float time([[:space:]]*\))/float _t\1/g' \
     -e 's/float round\(/float _round(/g' \
     -e 's/float sinh\(/float _sinh(/g' \
     -e 's/float cosh\(/float _cosh(/g' \
     -e 's/([^_a-zA-Z0-9])round\(/\1_round(/g' \
     -e 's/([^_a-zA-Z0-9])sinh\(/\1_sinh(/g' \
     -e 's/([^_a-zA-Z0-9])cosh\(/\1_cosh(/g' \
+    -e 's/sampler2D bb/sampler2D backbuffer/g' \
+    -e 's/texture2D\(bb,/texture(backbuffer,/g' \
+    -e 's/texture\(bb,/texture(backbuffer,/g' \
+    -e 's/([^_a-zA-Z0-9])bb([^_a-zA-Z0-9])/\1backbuffer\2/g' \
+    -e 's/float filter([[:space:]]*=)/float _filter\1/g' \
+    -e 's/([^_a-zA-Z0-9])filter([[:space:]]*[=\),;+*/-])/\1_filter\2/g' \
     "$INPUT" >> "$WRAPPED"
 
 # Compile to SPIR-V
